86e4ce6ba12a63bf11145aee7777b744ed8719ae
julien garcia <ju.garcia38@yahoo.fr>
2021-03-10 18:41:45 +0100

    urlHash fix

--- app/javascript/components/activeMenu.js
+++ app/javascript/components/activeMenu.js
@@ -1,13 +1,20 @@
 const selectNavbarItem = document.querySelectorAll('#menu li a');
 
 const activeClass = () => {
+  // const test = "#";
+  // const text = "Les # meilleurs viennent avec moi";
+
+  // const rplce = text.replace(test, '10');
+  // console.log(rplce);
   selectNavbarItem.forEach(item => {
     item.addEventListener('click', (e) => {
       const urlHash = item.innerHTML.toLowerCase();
+      const textWithoutSpace = urlHash.replace(" ", "-");
+
       clearActiveClass();
       item.classList.add('active');
       e.preventDefault();
-      window.location.hash = '#' + urlHash;
+      window.location.hash = '#' + textWithoutSpace;
     })
   })
 };
